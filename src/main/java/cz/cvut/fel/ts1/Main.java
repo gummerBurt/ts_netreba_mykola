b0ef593c5942b978505546705a1814899b9e5583
Your_Name_Here <your_gitlab_email@set.here>
2023-03-02 16:40:01 +0100

    test

--- src/main/java/cz/cvut/fel/ts1/Main.java
+++ src/main/java/cz/cvut/fel/ts1/Main.java
@@ -3,18 +3,18 @@ package cz.cvut.fel.ts1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Factorial of 5 (R): "+factorialRecursive(5));
+//        System.out.println("Factorial of 5 (R): "+factorialRecursive(5));
         System.out.println("Factorial of 5 (I): "+factorialIterative(5));
     }
 
-    public static int factorialRecursive(int n){
-        if(n <= 1){
-            return 1;
-        }
-        else{
-            return n*factorialRecursive(n-1);
-        }
-    }
+//    public static int factorialRecursive(int n){
+//        if(n <= 1){
+//            return 1;
+//        }
+//        else{
+//            return n*factorialRecursive(n-1);
+//        }
+//    }
 
     public static int factorialIterative(int n){
         int result=1,i=1;
